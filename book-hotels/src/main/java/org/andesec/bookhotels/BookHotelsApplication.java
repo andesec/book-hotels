10573f82fc76a09ad9dc08c723bb5db16ddefbfd
Ande <andesec1@gmail.com>
2025-01-09 17:57:40 +0500

    Initial Spring Boilerplate

---
+++ book-hotels/src/main/java/org/andesec/bookhotels/BookHotelsApplication.java
@@ -0,0 +1,13 @@
+package org.andesec.bookhotels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookHotelsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BookHotelsApplication.class, args);
+  }
+
+}
---
+++ book-hotels/src/test/java/org/andesec/bookhotels/BookHotelsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.andesec.bookhotels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookHotelsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
